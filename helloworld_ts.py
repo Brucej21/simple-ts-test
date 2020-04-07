ac5b4f51e10d4ada6731d1217e02f438ce52d82c
brucej21 <brucejenkins21@gmail.com>
2020-04-07 14:59:54 +1000

    test

--- helloworld_ts.py
+++ helloworld_ts.py
@@ -2,5 +2,16 @@
 print("this is now working")
 
 import subprocess
-#subprocess.call("command1")
+#produce reference artifact
 subprocess.call(["touch", "testABC.txt"])
+
+
+def double(x):
+    y = x+x
+    return y
+
+
+if __name__ == '__main__':
+    a = 2
+    b = double(a)
+    print(b)
---
+++ tests_tc/test_tc_one.py
@@ -0,0 +1,5 @@
+import helloworld_ts
+
+def test_double():
+    result = helloworld_ts.double(5)
+    assert result == 10
